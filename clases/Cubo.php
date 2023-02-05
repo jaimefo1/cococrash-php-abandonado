3a856b34a5aa21348b188ef2aa2afe7797635f73
JAIME FERNANDEZ ORTEGA <jaimefo1@gmail.com>
2023-02-05 11:54:37 +0100

    05/02/2023 creo que me voy a pasar a javascript...

--- clases/Cubo.php
+++ clases/Cubo.php
@@ -1,38 +1,52 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Cubo
  *
  * @author usuario
  */
-
 class Cubo {
+
     //contiene la definición del cubo, cada elemento del array indica una cara
     //según el dado típico, y contiene un array con el nº de cara con la que 
     //forma arista
     const
-         DEF_CUBO = array (
-             [5,3,2,4], [1,3,6,4], [1,5,6,2], [1,2,6,5], [1,4,6,3], [2,3,5,4] );
-    //almacena la pieza que va en cada cara
-    private $caras = array(6); //contendrá 6 piezas.
-    
-    public function __construct(){
+            DEF_CUBO = array(
+        [5, 3, 2, 4], [1, 3, 6, 4], [1, 5, 6, 2], [1, 2, 6, 5], [1, 4, 6, 3], [2, 3, 5, 4]);
+
+    //almacena las piezas que se van a utilizar para hacer el cubo
+    private $piezas = array();
+    //contendrá el número de la pieza que va en cada cara.
+    private $caras = array(6);
+
+    public function __construct($piezas) {
         //inicializamos el array de $caras
-        for ($n = 0; $n < 7; $n++) {
-            $caras[$n]= null;
-        }
-    }
-    
-    //devuelve la Arista $nArista de la cara $nCara
-    private function getArista($nCara,$nArista){
-        //esto es un comentario
-    }
-       
+        $caras = array_fill(0, 6, 0);
     }
 
+    //muestra en pantalla las piezas y posiciones de las caras puestas
+    private function tablaPiezasCubo() {
+        
+    }
+
+    //devuelve en $arista la arista $nArista de la  cara $nCara del cubo }
+    private function getArista($nCara, $nArista) {
+        $arista = new Arista();
+    }
+
+    //genera una pieza hipotética de la cara $nCara del Cubo
+    private function generaCaraCubo($nCara) {
+        $pieza = new Pieza(0, $this->$color);
+    }
+
+    //muestra en pantalla las 6 caras del CUBO, así como las piezas colocadas
+    private function ponCubo() {
+        
+    }
+
+    //nos devuelve verdad si la pieza $nPieza no ha sido utilizada
+    private function libre($nPieza) {
+        
+    }
+
+}
